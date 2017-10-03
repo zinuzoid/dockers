3820313a190033c4ab328b9e4978066d88a96b21
Jetsada Machom <zinuzoid@gmail.com>
2017-10-03 13:00:07 +0700

    Update http-request-dump

--- http-request-dump/main.go
+++ http-request-dump/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+        hostname, _ := os.Hostname()
+        fmt.Fprintf(w, "Powered on %v\n", hostname)
+
 	r.Write(w)
 }
 
